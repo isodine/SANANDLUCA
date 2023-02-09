da2d51790ec0de5e9b4c9a53fc8dfe55e9c31e32
TsuchiyaSuzuno <122pg0023@tokyo.amg.ac.jp>
2023-02-09 14:49:47 +0900

    引っ越し中

--- Game/Game/source/Enemy.cpp
+++ Game/Game/source/Enemy.cpp
@@ -12,8 +12,6 @@ void Enemy::Initialize() {
 	slimeAttackFlag = false;
 	slimeSerch = false;
 	slimeHitFlag = false;
-
-	slimeHandle = MV1LoadModel("res/slime_multimotion.mv1");
 }
 
 void Enemy::Terminate() {
@@ -21,14 +19,14 @@ void Enemy::Terminate() {
 }
 
 void Enemy::Process() {
-	
+
 }
 
 void Enemy::Render() {
 
 }
 
-void Enemy::Slime(VECTOR SanPos, VECTOR LkaPos, int HandleMap, float speed) {
+void Enemy::Slime(VECTOR SanPos, VECTOR LkaPos, int Handle, int HandleMap, float speed) {
 
 	_rotationMatrix = MMult(MMult(MGetRotX(slimeDir.x), MGetRotY(slimeDir.y)), MGetRotZ(slimeDir.z));
 
@@ -40,18 +38,18 @@ void Enemy::Slime(VECTOR SanPos, VECTOR LkaPos, int HandleMap, float speed) {
 	MV1_COLL_RESULT_POLY_DIM hitPolyDim1;
 	MV1_COLL_RESULT_POLY_DIM hitPolyDim2;
 
-	MV1SetupCollInfo(slimeHandle, 2, 8, 8, 8);
+	MV1SetupCollInfo(Handle, 2, 8, 8, 8);
 
-	hitPolyDim1 = MV1CollCheck_Capsule(slimeHandle, 2,
+	hitPolyDim1 = MV1CollCheck_Capsule(Handle, 2,
 		VGet(SanPos.x, SanPos.y + 30, SanPos.z), VGet(SanPos.x, SanPos.y + 75, SanPos.z), 30.0f);
-	hitPolyDim2 = MV1CollCheck_Capsule(slimeHandle, 2,
+	hitPolyDim2 = MV1CollCheck_Capsule(Handle, 2,
 		VGet(LkaPos.x, LkaPos.y + 30, LkaPos.z), VGet(LkaPos.x, LkaPos.y + 75, LkaPos.z), 30.0f);
 
 	if (hitPolyDim1.HitNum >= 1 || hitPolyDim2.HitNum >= 1) {
 		slimeHitFlag = true;
 	}
 	else {
-		
+
 	}
 
 	sanDistance = VSize(VSub(SanPos, slimePos));
@@ -72,7 +70,7 @@ void Enemy::Slime(VECTOR SanPos, VECTOR LkaPos, int HandleMap, float speed) {
 		slimeDir.y = atan2(lkaPos.x * -1, lkaPos.z * -1);
 		_status = STATUS::ATTACK;
 		slimeAttackFlag = true;
-		
+
 	}
 
 	else if (sanDistance < lkaDistance && !slimeAttackFlag)
@@ -80,7 +78,7 @@ void Enemy::Slime(VECTOR SanPos, VECTOR LkaPos, int HandleMap, float speed) {
 		slimeSerch = true;
 		slimeDir.y = atan2(sanPos.x * -1, sanPos.z * -1);
 		_status = STATUS::WALK_KAI;
-		
+
 		slimePos = VAdd(slimePos, VScale(forward, speed));
 	}
 	else if (sanDistance >= lkaDistance && !slimeAttackFlag)
@@ -105,26 +103,26 @@ void Enemy::Slime(VECTOR SanPos, VECTOR LkaPos, int HandleMap, float speed) {
 	else {
 		// アニメーションがアタッチされていたら、デタッチする
 		if (AttachAnim != -1) {
-			MV1DetachAnim(slimeHandle, AttachAnim);
+			MV1DetachAnim(Handle, AttachAnim);
 			AttachAnim = -1;
 		}
 		// ステータスに合わせてアニメーションをアタッチする
 		switch (_status) {
 		case STATUS::WALK_KAI:
-			AttachAnim = MV1AttachAnim(slimeHandle, 2, -1, FALSE);//歩きモーションをアタッチする
+			AttachAnim = MV1AttachAnim(Handle, 2, -1, FALSE);//歩きモーションをアタッチする
 			break;
 		case STATUS::ATTACK:
-			AttachAnim = MV1AttachAnim(slimeHandle, 0, -1, FALSE);//攻撃モーションをアタッチする
+			AttachAnim = MV1AttachAnim(Handle, 0, -1, FALSE);//攻撃モーションをアタッチする
 			break;
 		case STATUS::HIT:
-			AttachAnim = MV1AttachAnim(slimeHandle, 1, -1, FALSE);//ダメージモーションをアタッチする
+			AttachAnim = MV1AttachAnim(Handle, 1, -1, FALSE);//ダメージモーションをアタッチする
 			break;
 		}
-		TotalTime = MV1GetAttachAnimTotalTime(slimeHandle, AttachAnim);
+		TotalTime = MV1GetAttachAnimTotalTime(Handle, AttachAnim);
 		PlayTime = 0.0f;
 	}
 
-	MV1SetAttachAnimTime(slimeHandle, AttachAnim, PlayTime);
+	MV1SetAttachAnimTime(Handle, AttachAnim, PlayTime);
 }
 
 void Enemy::Spider(VECTOR SanPos, VECTOR LkaPos, VECTOR SpiderPos, int Handle, float speed) {
@@ -143,14 +141,14 @@ void Enemy::SlimeJump() {
 	VECTOR forward{ VTransform({0.0f,0.0f,-1.0f},_rotationMatrix) };
 	VECTOR up{ 0.0f,0.1f,0.0f };
 	float spd = 2.0f;
-	
+
 
 	forward = VScale(forward, spd);
 	slimecount += 1;
 	if (slimeSerch == true && slimecount == 1) {
 		slimeDir.y = atan2(sanPos.x * -1, sanPos.z * -1);
 	}
-	else if(slimeSerch == false && slimecount == 1) {
+	else if (slimeSerch == false && slimecount == 1) {
 		slimeDir.y = atan2(lkaPos.x * -1, lkaPos.z * -1);
 	}
 	if (slimecount <= 20 && slimecount != 0 && slimeAttackFlag == true) {
@@ -165,11 +163,11 @@ void Enemy::SlimeJump() {
 			slimePos = VSub(slimePos, VScale(forward, 4.0f));
 			slimePos = VSub(slimePos, VScale(up, 20.0f));
 		}
-		else if(slimeHitFlag == false) {
+		else if (slimeHitFlag == false) {
 			slimePos = VAdd(slimePos, VScale(forward, 0.5f));
 			slimePos = VSub(slimePos, VScale(up, 20.0f));
 		}
-		
+
 	}
 	else if (slimecount == 61) {
 		slimecount = 0;
@@ -178,12 +176,12 @@ void Enemy::SlimeJump() {
 	}
 }
 
-void Enemy::SlimeRender(VECTOR Pos) {
-	MV1SetPosition(slimeHandle, Pos);
-	MV1SetScale(slimeHandle, VGet(3.0f, 3.0f, 3.0f));
-	MV1SetRotationXYZ(slimeHandle, slimeDir);
-	MV1DrawModel(slimeHandle);
-	
+void Enemy::SlimeRender(VECTOR Pos, int Handle) {
+	MV1SetPosition(Handle, Pos);
+	MV1SetScale(Handle, VGet(3.0f, 3.0f, 3.0f));
+	MV1SetRotationXYZ(Handle, slimeDir);
+	MV1DrawModel(Handle);
+
 }
 
 void Enemy::SpiderRender(VECTOR Pos, int Handle) {
