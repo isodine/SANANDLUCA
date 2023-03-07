b2c134d7fa4583937111471621bd8665fbeeabd6
isodine <122PG0020@tokyo.amg.ac.jp>
2023-03-07 19:03:34 +0900

    ヴぁ

--- Game/Game/source/SanBomb.h
+++ Game/Game/source/SanBomb.h
@@ -1,30 +1,14 @@
 #pragma once
-class SAN;
-class Sanbomb
+#include "PlayerBomb.h"
+
+class SanBomb :public PlayerBomb
 {
 public:
-	Sanbomb();
-	~Sanbomb();
-
-	void Init();
+	SanBomb();
+	~SanBomb();
+	void Initialize(SAN& san);
 	void Update(SAN& san);
 	void Render();
-	void bombthrow();
-private:
-	int		_effectResourceHandle;		// エフェクトファイルをロードするハンドル
-	int		_playingEffectHandle;		// ロードしたエフェクトファイルから、エフェクトを生成したもの
-	int oldcount;
-	float PlayTime = 0.0f;
-	int   _gKeyEf, _gTrgEf, _KeyEf, _TrgEf;
-	int _isEffect = 0;
-	float _throw;
-	float _hight;
-	int _isthrow = 0;
-
-	// エフェクト位置
-	float _position_x;
-	float _position_y;
-	float _position_z;
-
-
+	void Move();
+	void Throw(SAN& san);
 };
