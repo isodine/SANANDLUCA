9d8c376797c8e3a3a08a796b0e88c222a2dfbbb2
Rudra_AMG <121pg0029@tokyo.amg.ac.jp>
2023-03-07 16:36:18 +0900

    コミット

--- Game/Game/source/sanbomb.h
+++ Game/Game/source/sanbomb.h
@@ -9,11 +9,16 @@ public:
 	void Init();
 	void Update(SAN& san);
 	void Render();
+	void bombthrow();
 private:
-	int		_effectResourceHandle;		// エフェクトファイルをロードするハンドル
-	int		_playingEffectHandle;		// ロードしたエフェクトファイルから、エフェクトを生成したもの
+	int _effectResourceHandle;		// エフェクトファイルをロードするハンドル
+	int _playingEffectHandle;		// ロードしたエフェクトファイルから、エフェクトを生成したもの
 	int oldcount;
 	float PlayTime = 0.0f;
 	int _isEffect = 0;
+	int _isthrow = 0;
 	int IsPlaying = -1;
+	int _KeyEf, _TrgEf;
+	float _hight, _throw;
+	float _position_x, _position_y, _position_z;
 };
